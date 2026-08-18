cf601bb4d9aa0633122577af0b0418bde137f4da
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-18 17:58:49 +1000

    Configure Java 21 test build

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    java
+}
+
+group = "io.autoptu"
+version = "0.1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.11.4"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
