4d66d7dbbca79bd80778169e2525308099ce3f91
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-28 08:01:24 +1000

    FAC-002 register Minecraft shop catalogue

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -19,6 +19,7 @@ import io.autoptu.cobblemon.fabric.rpg.FabricPartyLeadRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPartySummaryRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPlayerStatusRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPokemonDetailRuntime;
+import io.autoptu.cobblemon.fabric.rpg.FabricShopCatalogueRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricStarterCatalogueRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricStarterSelectionRuntime;
 import io.autoptu.cobblemon.fabric.world.CedarMeadowRuntime;
@@ -55,6 +56,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         FabricPokemonDetailRuntime.register();
         FabricStarterCatalogueRuntime.register();
         FabricStarterSelectionRuntime.register();
+        FabricShopCatalogueRuntime.register();
         VisibleWildPokemonEncounterRuntime.register();
         CedarMeadowRuntime.register();
         OurosSignatureBuildRuntime.register();
