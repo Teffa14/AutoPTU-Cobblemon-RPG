59cf1ae69c2866a4104658e3b9e6e09f4dacbaba
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-27 21:54:26 +1000

    Register world encounter trigger runtime

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -19,6 +19,7 @@ import io.autoptu.cobblemon.fabric.rpg.FabricPokemonDetailRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricStarterCatalogueRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricStarterSelectionRuntime;
 import io.autoptu.cobblemon.fabric.world.CedarMeadowRuntime;
+import io.autoptu.cobblemon.fabric.world.FabricWorldEncounterTriggerRuntime;
 import io.autoptu.cobblemon.fabric.world.OurosBuildManifestExportRuntime;
 import io.autoptu.cobblemon.fabric.world.OurosGrandPalaceV4ManifestExportRuntime;
 import io.autoptu.cobblemon.fabric.world.OurosSignatureBuildRuntime;
@@ -48,6 +49,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         FabricPokemonDetailRuntime.register();
         FabricStarterCatalogueRuntime.register();
         FabricStarterSelectionRuntime.register();
+        FabricWorldEncounterTriggerRuntime.register();
         CedarMeadowRuntime.register();
         OurosSignatureBuildRuntime.register();
         OurosBuildManifestExportRuntime.registerIfEnabled();
