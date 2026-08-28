a4f763dade77a5aaa9f315eb7b8f3d369363afd3
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-28 12:54:04 +1000

    Crafting: register fallback command runtime

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -14,6 +14,7 @@ import io.autoptu.cobblemon.fabric.presentation.CobblemonLiveReactionPlaybackSmo
 import io.autoptu.cobblemon.fabric.presentation.CobblemonLiveRelocationSmoke;
 import io.autoptu.cobblemon.fabric.rpg.FabricCraftIngredientDepositRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricCraftingAssessmentRuntime;
+import io.autoptu.cobblemon.fabric.rpg.FabricCraftingCommandRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricCraftingWorkstationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntimeSmoke;
@@ -61,6 +62,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         FabricStarterSelectionRuntime.register();
         FabricTrainerPtuActionRuntime.register();
         FabricCraftingAssessmentRuntime.register();
+        FabricCraftingCommandRuntime.register();
         FabricCraftIngredientDepositRuntime.register();
         FabricCraftingWorkstationRuntime.register();
         VisibleWildPokemonEncounterRuntime.register();
