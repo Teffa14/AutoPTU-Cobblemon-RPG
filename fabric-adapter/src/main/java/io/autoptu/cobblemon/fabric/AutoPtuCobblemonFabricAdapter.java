69a8ec4a9de9559ec98d72a43271489858dbae9f
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-29 09:59:34 +1000

    RPG: register NPC dialogue runtime

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -20,6 +20,7 @@ import io.autoptu.cobblemon.fabric.rpg.FabricCraftingWorkstationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricFieldCampRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntimeSmoke;
+import io.autoptu.cobblemon.fabric.rpg.FabricNpcDialogueRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPartyHealingRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPartyLeadRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPartySummaryRuntime;
@@ -72,6 +73,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         FabricShopRuntime.register();
         FabricShopCounterRuntime.register();
         FabricShopCounterRuntimeSmoke.registerIfEnabled();
+        FabricNpcDialogueRuntime.register();
         FabricPokemonStorageRuntime.register();
         FabricPokemonStorageTerminalRuntime.register();
         FabricPokemonStorageTerminalRuntimeSmoke.registerIfEnabled();
