1efe48949deaca8fc5f21ed036420f9714dff759
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-29 21:32:13 +1000

    RPG: register world authority protection

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -37,6 +37,7 @@ import io.autoptu.cobblemon.fabric.rpg.FabricPokemonStorageTerminalRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricPokemonStorageTerminalRuntimeSmoke;
 import io.autoptu.cobblemon.fabric.rpg.FabricQuestJournalRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricRpgCalendarRuntime;
+import io.autoptu.cobblemon.fabric.rpg.FabricRpgProtectionRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricShopCounterRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricShopCounterRuntimeSmoke;
 import io.autoptu.cobblemon.fabric.rpg.FabricShopRuntime;
@@ -67,6 +68,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         if (!FabricLoader.getInstance().isModLoaded("cobblemon")) {
             throw new IllegalStateException("Cobblemon runtime is required by the AutoPTU adapter");
         }
+        FabricRpgProtectionRuntime.register();
         FabricFirstJoinOnboardingRuntime.register();
         PlayableBattleTestRuntime.register();
         FabricBattleChoiceRuntime.register();
