ff740bfe45a1d2405a3ddff0dca99e13a95150c2
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-08-29 15:00:36 +1000

    RPG: register item storage runtime

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/AutoPtuCobblemonFabricAdapter.java
@@ -20,6 +20,7 @@ import io.autoptu.cobblemon.fabric.rpg.FabricCraftingWorkstationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricFieldCampRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricHealingStationRuntimeSmoke;
+import io.autoptu.cobblemon.fabric.rpg.FabricItemStorageRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricNpcDialogueRuntime;
 import io.autoptu.cobblemon.fabric.rpg.FabricNpcDialogueRuntimeSmoke;
 import io.autoptu.cobblemon.fabric.rpg.FabricPartyHealingRuntime;
@@ -71,6 +72,7 @@ public final class AutoPtuCobblemonFabricAdapter implements ModInitializer {
         FabricPartyLeadRuntime.register();
         FabricPlayerStatusRuntime.register();
         FabricBagRuntime.register();
+        FabricItemStorageRuntime.register();
         FabricWalletRuntime.register();
         FabricShopRuntime.register();
         FabricShopCounterRuntime.register();
