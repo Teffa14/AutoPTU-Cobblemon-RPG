a03f605705f619cc9942b33a8e16010e9b797c5d
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-09-02 22:01:58 +1000

    Test persistent lever redstone propagation

--- fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/rpg/FabricWorldEventProjectionRuntimeSmoke.java
+++ fabric-adapter/src/main/java/io/autoptu/cobblemon/fabric/rpg/FabricWorldEventProjectionRuntimeSmoke.java
@@ -9,10 +9,13 @@ import net.minecraft.block.Blocks;
 import net.minecraft.block.DoorBlock;
 import net.minecraft.block.FenceGateBlock;
 import net.minecraft.block.LeverBlock;
+import net.minecraft.block.RedstoneLampBlock;
 import net.minecraft.block.RespawnAnchorBlock;
 import net.minecraft.block.TrapdoorBlock;
+import net.minecraft.block.enums.BlockFace;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.BlockPos;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -38,11 +41,15 @@ public final class FabricWorldEventProjectionRuntimeSmoke {
         ServerWorld world = server.getOverworld();
         BlockPos shrineAnchor = world.getSpawnPos().up(36);
         BlockPos switchAnchor = shrineAnchor.east(2);
+        BlockPos switchSupport = switchAnchor.down();
+        BlockPos switchLamp = switchSupport.east();
         BlockPos doorAnchor = shrineAnchor.east(4);
         BlockPos trapdoorAnchor = shrineAnchor.east(6);
         BlockPos fenceGateAnchor = shrineAnchor.east(8);
         BlockState originalShrine = world.getBlockState(shrineAnchor);
         BlockState originalSwitch = world.getBlockState(switchAnchor);
+        BlockState originalSwitchSupport = world.getBlockState(switchSupport);
+        BlockState originalSwitchLamp = world.getBlockState(switchLamp);
         BlockState originalDoor = world.getBlockState(doorAnchor);
         BlockState originalTrapdoor = world.getBlockState(trapdoorAnchor);
         BlockState originalFenceGate = world.getBlockState(fenceGateAnchor);
@@ -86,14 +93,25 @@ public final class FabricWorldEventProjectionRuntimeSmoke {
                 throw new IllegalStateException("world-event projection mutated a non-shrine block");
             }
 
+            world.setBlockState(switchSupport, Blocks.GOLD_BLOCK.getDefaultState(), Block.NOTIFY_ALL);
+            world.setBlockState(
+                    switchLamp,
+                    Blocks.REDSTONE_LAMP.getDefaultState().with(RedstoneLampBlock.LIT, false),
+                    Block.NOTIFY_ALL
+            );
             world.setBlockState(
                     switchAnchor,
-                    Blocks.LEVER.getDefaultState().with(LeverBlock.POWERED, false),
+                    Blocks.LEVER.getDefaultState()
+                            .with(Properties.BLOCK_FACE, BlockFace.FLOOR)
+                            .with(LeverBlock.POWERED, false),
                     Block.NOTIFY_ALL
             );
             FabricCanonicalWorldInteractionRuntime.projectSwitchState(world, switchAnchor, activatedSwitch);
             if (!world.getBlockState(switchAnchor).get(LeverBlock.POWERED)) {
                 throw new IllegalStateException("activated canonical switch did not project to powered Minecraft state");
+            }
+            if (!world.getBlockState(switchLamp).get(RedstoneLampBlock.LIT)) {
+                throw new IllegalStateException("persistent lever projection did not propagate strong power through its support block");
             }
 
             FabricCanonicalWorldInteractionRuntime.projectSwitchState(world, switchAnchor, activatedSwitch);
@@ -158,6 +176,8 @@ public final class FabricWorldEventProjectionRuntimeSmoke {
         } finally {
             world.setBlockState(shrineAnchor, originalShrine, Block.NOTIFY_ALL);
             world.setBlockState(switchAnchor, originalSwitch, Block.NOTIFY_ALL);
+            world.setBlockState(switchSupport, originalSwitchSupport, Block.NOTIFY_ALL);
+            world.setBlockState(switchLamp, originalSwitchLamp, Block.NOTIFY_ALL);
             world.setBlockState(doorAnchor, originalDoor, Block.NOTIFY_ALL);
             world.setBlockState(trapdoorAnchor, originalTrapdoor, Block.NOTIFY_ALL);
             world.setBlockState(fenceGateAnchor, originalFenceGate, Block.NOTIFY_ALL);
