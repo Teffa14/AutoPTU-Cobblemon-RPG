3f42e65dac07c614774f061a7c21727c93b42615
Teffa14 <98290252+Teffa14@users.noreply.github.com>
2026-09-04 00:43:58 +1000

    Add migration ecology runtime phase model

---
+++ src/main/java/io/autoptu/cobblemon/ecology/MigrationPhase.java
@@ -0,0 +1,12 @@
+package io.autoptu.cobblemon.ecology;
+
+public enum MigrationPhase {
+    PREPARING,
+    DEPARTING,
+    IN_TRANSIT,
+    STOPOVER,
+    ARRIVING,
+    SEASONAL_RESIDENCE,
+    RETURNING,
+    COMPLETE
+}
